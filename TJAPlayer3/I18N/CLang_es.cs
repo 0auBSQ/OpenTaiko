33259ad04b5fcb8f40a6315db8a731ac413d0e25
Funnym0th <52089905+funnym0th@users.noreply.github.com>
2021-11-09 09:14:57 -0300

    Add Spanish translation (#53)
    * Update CLang_en.cs
    
    * Update CLang_en.cs
    
    * Update CLang_en.cs
    
    * Update CLang_en.cs
    
    * Update CLang_en.cs
    
    * Update CLang_en.cs
    
    * Add files via upload
    
    * Update CLang_es.cs
    
    Co-authored-by: Nukecat <52089905+MrKotorrowo@users.noreply.github.com>

---
+++ TJAPlayer3/I18N/CLang_es.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TJAPlayer3
+{
+    internal class CLang_es : ILang
+    {
+        string ILang.GetString(int idx)
+        {
+            if (!dictionnary.ContainsKey(idx))
+                return "[!] No se ha encontrado el índice en el diccionario.";
+
+            return dictionnary[idx];
+        }
+
+
+        private static readonly Dictionary<int, string> dictionnary = new Dictionary<int, string>
+        {
+            [0] = "Cambia el idioma que se usa\nen el juego y menús.",
+            [1] = "Idioma del sistema",
+            [2] = "<< Volver al menú",
+            [3] = "Volver al menú de la izquierda.",
+            [4] = "Recargar datos de canciones",
+            [5] = "Recarga y actualiza la lista de canciones.",
+            [6] = "Número de jugadores",
+            [7] = "Cambia el número de jugadores.\nAjustarlo a 2 permite jugar\ncanciones regulares a dos jugadores dividiendo \nla pantalla a la mitad.",
+            [8] = "Riesgoso",
+            [9] = "Modo riesgoso:\nDéjalo por sobre 1, en caso de que quieras especificar\nel número de Malo/Fallos para considerar el intento\nfallido.\nDejar en 0 para deshabilitar el modo riesgoso.",
+            [10] = "Velocidad de la canción",
+            [11] = "Cambia la velocidad de la canción.\n" +
+                "Por ejemplo, puedes jugar a mitad de\n" +
+                "velocidad ajustando el valor PlaySpeed = 0.500\n" +
+                "para practicar.\n" +
+                "\n" +
+                "Nota: También cambia el tono de la canción.\n" +
+                "Si el siguiente valor se encuentra así: TimeStretch=ON, puede haber\n" +
+                "lag de audio si se usa en menos de x0.900.",
+            [16] = "Tipo de interfaz",
+            [17] = "Puedes cambiar la interfaz de las canciones \nmostradas en la pantalla de selección.\n" +
+                "0 : Regular (Diagonal de arriba hacia abajo)\n" +
+                "1 : Vertical\n" +
+                "2 : Diagonal de abajo hacia arriba\n" +
+                "3 : Medio circulo hacia la derecha\n" +
+                "4 : Medio circulo hacia la izquierda",
+
+            [100] = "Modo Taiko",
+            [101] = "Desafíos del Dojo",
+            [102] = "Torres Taiko",
+            [103] = "Tienda",
+            [104] = "Aventura Taiko",
+            [105] = "Mi Habitación",
+            [106] = "Ajustes",
+            [107] = "Salir",
+
+            [150] = "Juega tus canciones\nfavoritas a tu propio gusto!",
+            [151] = "Juega varias canciones seguidas de\npruebas desafiantes\npara obtener el rango Aprobado!",
+            [152] = "Juega canciones largas con un\nnumero de vidas limitado y llega\na la punta de la torre!",
+            [153] = "Compra nuevas canciones, petit-chara o personajes\nusando las medallas que ganaste jugando!",
+            [154] = "Atraviesa varios obstáculos y\ndesbloquea nuevo contenido!",
+            [155] = "Cambia la información de tu placa\n o el aspecto de tu personaje!",
+            [156] = "Cambia tu estilo de juego\n o ajustes generales!",
+            [157] = "Salir del juego.\nHasta la próxima!",
+
+            [1000] = "Piso alcanzado",
+            [1001] = "P",
+            [1002] = "P",
+            [1003] = "Puntuación",
+        };
+    }
+}
