b4d6b13a214b79e8df4479ef37948dfc6dd9d491
0aubsq <0aubsq@gmail.com>
2022-01-10 12:03:13 +0100

    First textures for default skin

--- TJAPlayer3/Stages/06.SongLoading/CStage曲読み込み.cs
+++ TJAPlayer3/Stages/06.SongLoading/CStage曲読み込み.cs
@@ -258,7 +258,7 @@ namespace TJAPlayer3
 
             if (TJAPlayer3.stage選曲.n確定された曲の難易度[0] != (int)Difficulty.Dan)
 			{
-				#region [ 背景、音符＋タイトル表示 ]
+				#region [ Loading screen (except dan) ]
 				//-----------------------------
 				this.ct曲名表示.t進行();
 
