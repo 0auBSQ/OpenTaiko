9dca8ee888d637c0b77dc8a49d5ad697201a8569
L1m0n3 <13360351+l1m0n3@users.noreply.github.com>
2022-02-20 16:20:09 +0900

    Fix build script ・It's now compatible with x86 windows. ・Escaped parentheses in the script which was causing the issue.

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set VSWHERE="!ProgramFiles(x86)!\Microsoft Visual Studio\Installer\vswhere.exe"
+if defined ProgramFiles(x86) (
+    set VSWHERE="!ProgramFiles(x86)!\Microsoft Visual Studio\Installer\vswhere.exe"
+) else (
+    set VSWHERE="!ProgramFiles!\Microsoft Visual Studio\Installer\vswhere.exe"
+)
 
 if exist !VSWHERE! (
     for /f "usebackq delims=" %%i in (`!VSWHERE! -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
@@ -11,10 +15,10 @@ if exist !VSWHERE! (
     if exist !MSBUILD! (
         !MSBUILD! TJAPlayer3.sln /t:build -restore /p:configuration=release /p:platform=x86
     ) else (
-        echo MSBuild is not installed. Please read: https://github.com/l1m0n3/OpenTaiko/wiki/How-to-build-OpenTaiko-without-using-Visual-Studio-(on-Windows)
+        echo MSBuild is not installed. Please read: https://github.com/l1m0n3/OpenTaiko/wiki/How-to-build-OpenTaiko-without-using-Visual-Studio-^(on-Windows^)
     )
 ) else (
-    echo Visual Studio Installer is not installed. Please read: https://github.com/l1m0n3/OpenTaiko/wiki/How-to-build-OpenTaiko-without-using-Visual-Studio-(on-Windows)
+    echo Visual Studio Installer is not installed. Please read: https://github.com/l1m0n3/OpenTaiko/wiki/How-to-build-OpenTaiko-without-using-Visual-Studio-^(on-Windows^)
 )
 
 pause
