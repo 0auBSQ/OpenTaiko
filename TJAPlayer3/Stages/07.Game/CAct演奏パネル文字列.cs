da61e7e549515f27b035fb03c5f0518fcca2cc0b
0aubsq <0aubsq@gmail.com>
2022-04-02 23:56:24 +0200

    Add english default in-game genre panels + add back 'the removed one' for japanese players

--- TJAPlayer3/Stages/07.Game/CAct演奏パネル文字列.cs
+++ TJAPlayer3/Stages/07.Game/CAct演奏パネル文字列.cs
@@ -29,17 +29,28 @@ namespace TJAPlayer3
         private readonly Dictionary<string, Color4> tTagDict = new Dictionary<string, Color4>
         {
             ["アニメ"] = new Color4(tToArgb(253, 145, 208)),
+            ["Anime"] = new Color4(tToArgb(253, 145, 208)),
             ["クラシック"] = new Color4(tToArgb(221, 172, 4)),
+            ["Classical"] = new Color4(tToArgb(221, 172, 4)),
             ["バラエティ"] = new Color4(tToArgb(32, 218, 56)),
+            ["Variety"] = new Color4(tToArgb(32, 218, 56)),
             ["どうよう"] = new Color4(tToArgb(254, 191, 3)),
             ["キッズ"] = new Color4(tToArgb(254, 191, 3)),
+            ["Children & Folk"] = new Color4(tToArgb(254, 191, 3)),
             ["ボーカロイド"] = new Color4(tToArgb(204, 207, 222)),
+            ["VOCALOID"] = new Color4(tToArgb(204, 207, 222)),
             ["Vocaloid"] = new Color4(tToArgb(204, 207, 222)),
             ["ゲームミュージック"] = new Color4(tToArgb(205, 138, 237)),
             ["ゲームバラエティ"] = new Color4(tToArgb(205, 138, 237)),
+            ["Game Music"] = new Color4(tToArgb(205, 138, 237)),
             ["J-POP"] = new Color4(tToArgb(68, 192, 209)),
+            ["POP"] = new Color4(tToArgb(68, 192, 209)),
+            ["ナムコオリジナル"] = new Color4(tToArgb(255, 70, 28)),
+            ["OpenTaikoオリジナル"] = new Color4(tToArgb(249, 255, 40)),
+            ["OpenTaiko Original"] = new Color4(tToArgb(249, 255, 40)),
             ["ポップス"] = new Color4(tToArgb(68, 192, 209)),
             ["太鼓タワー"] = new Color4(tToArgb(254, 191, 3)),
+            ["Taiko Towers"] = new Color4(tToArgb(254, 191, 3)), // Temporary, will use CLang
             ["段位道場"] = new Color4(tToArgb(42, 122, 169)),
         };
 
