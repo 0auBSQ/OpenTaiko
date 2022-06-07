9edf3e85618d124495c5758c235344ac06a5ca27
Takkkom <takukom@outlook.jp>
2022-06-07 14:05:34 +0900

    Script.luaを簡潔に

--- Test/System/SimpleStyle/Graphics/5_Game/5_Background/Dan/Up/0/Script.lua
+++ Test/System/SimpleStyle/Graphics/5_Game/5_Background/Dan/Up/0/Script.lua
@@ -1,16 +1,3 @@
-local fps = 0
-local deltaTime = 0
-local isClear = { false, false }
-local towerNightNum = 0
-
-function updateValues(_deltaTime, _fps, _isClear, _towerNightNum)
-    deltaTime = _deltaTime
-    fps = _fps
-    isClear = _isClear
-    towerNightNum = _towerNightNum
-    deltaTime = _deltaTime
-end
-
 local bgWidth_1 = 1323
 local bgWidth_2 = 1280
 local bgWidth_3 = 1280
--- Test/System/SimpleStyle/Graphics/5_Game/5_Background/Normal/Up/0/Script.lua
+++ Test/System/SimpleStyle/Graphics/5_Game/5_Background/Normal/Up/0/Script.lua
@@ -1,16 +1,3 @@
-local fps = 0
-local deltaTime = 0
-local isClear = { false, false }
-local towerNightNum = 0
-
-function updateValues(_deltaTime, _fps, _isClear, _towerNightNum)
-    deltaTime = _deltaTime
-    fps = _fps
-    isClear = _isClear
-    towerNightNum = towerNightNum
-    deltaTime = _deltaTime
-end
-
 local loopWidth = 328
 local loopHeight = 154
 
