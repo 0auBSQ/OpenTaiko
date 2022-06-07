4bc0d98a6ca3c5d5acbb228dce57ee47a9bbc011
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2022-06-07 08:32:27 +0200

    Keep documentation for function calls on Background Lua scripts

--- Test/System/SimpleStyle/Graphics/5_Game/5_Background/Sample/Template/Script.lua
+++ Test/System/SimpleStyle/Graphics/5_Game/5_Background/Sample/Template/Script.lua
@@ -1,3 +1,11 @@
+--func:DrawText(x, y, text);
+--func:DrawNum(x, y, num);
+--func:AddGraph("filename");
+--func:DrawGraph(x, y, filename);
+--func:SetOpacity(opacity, "filename");
+--func:SetScale(xscale, yscale, "filename");
+--func:SetColor(r, g, b, "filename");
+
 function clearIn(player)
 end
 
