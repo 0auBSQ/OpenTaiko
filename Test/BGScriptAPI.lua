24b5152f7690a28d5dd53fb5daeb9619561ffd64
Takkkom <76614532+Takkkom@users.noreply.github.com>
2022-06-21 12:30:16 +0900

    背景のlua関係でメモリリークをしていた問題の修正 (#262)

--- Test/BGScriptAPI.lua
+++ Test/BGScriptAPI.lua
@@ -1,7 +1,15 @@
+local playerCount = 0
+local p1IsBlue = false
+
 local fps = 0
 local deltaTime = 0
 local isClear = { false, false }
 local towerNightNum = 0
+
+function setConstValues(_playerCount, _p1IsBlue)
+    playerCount = _playerCount
+    p1IsBlue = _p1IsBlue
+end
 
 function updateValues(_deltaTime, _fps, _isClear, _towerNightNum)
     deltaTime = _deltaTime
