e1e5b651e4a17692f8e1b6127c78a1aa3cc1a76e
Takkkom <takukom@outlook.jp>
2023-02-11 13:47:39 +0900

    ScriptのTemplateを更新

--- Test/System/SimpleStyle/Graphics/5_Game/5_Background/Sample/Template/Script.lua
+++ Test/System/SimpleStyle/Graphics/5_Game/5_Background/Sample/Template/Script.lua
@@ -2,7 +2,11 @@
 --func:DrawNum(x, y, num)
 --func:AddGraph("filename")
 --func:DrawGraph(x, y, filename)
+--func:DrawRectGraph(x, y, rect_x, rect_y, rect_width, rect_height, filename)
+--func:DrawGraphCenter(x, y, filename)
+--func:DrawGraphRectCenter(x, y, rect_x, rect_y, rect_width, rect_height, filename)
 --func:SetOpacity(opacity, "filename")
+--func:SetRotation(angle, "fileName")
 --func:SetScale(xscale, yscale, "filename")
 --func:SetColor(r, g, b, "filename")
 
