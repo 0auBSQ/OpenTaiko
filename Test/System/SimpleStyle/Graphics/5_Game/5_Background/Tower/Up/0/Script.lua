adfd0571b6eca029f3a26fcfe504dc28e40e6225
Takkkom <takukom@outlook.jp>
2023-02-11 13:27:33 +0900

    いろんなステージでluaを使用できるように

--- Test/System/SimpleStyle/Graphics/5_Game/5_Background/Tower/Up/0/Script.lua
+++ Test/System/SimpleStyle/Graphics/5_Game/5_Background/Tower/Up/0/Script.lua
@@ -16,67 +16,67 @@ function clearOut(player)
 end
 
 function init()
-    func:AddGraph("0.png");
-    func:AddGraph("1.png");
-    func:AddGraph("2.png");
-    func:AddGraph("3.png");
-    func:AddGraph("4.png");
-    func:AddGraph("5.png");
-    func:AddGraph("6.png");
-    func:AddGraph("7.png");
+    func:AddGraph("0.png")
+    func:AddGraph("1.png")
+    func:AddGraph("2.png")
+    func:AddGraph("3.png")
+    func:AddGraph("4.png")
+    func:AddGraph("5.png")
+    func:AddGraph("6.png")
+    func:AddGraph("7.png")
 end
 
 function update()
-    bgScrollX_1 = bgScrollX_1 + (59.1 * deltaTime);
+    bgScrollX_1 = bgScrollX_1 + (59.1 * deltaTime)
     if bgScrollX_1 > bgWidth_1 then
-        bgScrollX_1 = 0;
+        bgScrollX_1 = 0
     end
     
-    bgScrollX_2 = bgScrollX_2 + (45.9 * deltaTime);
+    bgScrollX_2 = bgScrollX_2 + (45.9 * deltaTime)
     if bgScrollX_2 > bgWidth_2 then
-        bgScrollX_2 = 0;
+        bgScrollX_2 = 0
     end
     
-    bgScrollX_3 = bgScrollX_3 + (43.8 * deltaTime);
+    bgScrollX_3 = bgScrollX_3 + (43.8 * deltaTime)
     if bgScrollX_3 > bgWidth_3 then
-        bgScrollX_3 = 0;
+        bgScrollX_3 = 0
     end
     
-    bgScrollX_4 = bgScrollX_4 + (100 * deltaTime);
+    bgScrollX_4 = bgScrollX_4 + (100 * deltaTime)
     if bgScrollX_4 > bgWidth_4 + 200 then
-        bgScrollX_4 = 0;
+        bgScrollX_4 = 0
     end
     
-    bgScrollX_5 = bgScrollX_5 + (45.9 * deltaTime);
+    bgScrollX_5 = bgScrollX_5 + (45.9 * deltaTime)
     if bgScrollX_5 > bgWidth_5 then
-        bgScrollX_5 = 0;
+        bgScrollX_5 = 0
     end
 end
 
 
 function draw()
-    func:DrawGraph(0, 0, "0.png");
-    func:SetOpacity((towerNightNum * 255.0), "7.png");
-    func:DrawGraph(0, 0, "7.png");
+    func:DrawGraph(0, 0, "0.png")
+    func:SetOpacity((towerNightNum * 255.0), "7.png")
+    func:DrawGraph(0, 0, "7.png")
 
-    colorTmp = 0.5 + (1 - towerNightNum) * 0.5;
+    colorTmp = 0.5 + (1 - towerNightNum) * 0.5
 
-    func:SetColor(colorTmp, colorTmp, colorTmp, "1.png");
-    func:SetColor(colorTmp, colorTmp, colorTmp, "2.png");
-    func:SetColor(colorTmp, colorTmp, colorTmp, "3.png");
+    func:SetColor(colorTmp, colorTmp, colorTmp, "1.png")
+    func:SetColor(colorTmp, colorTmp, colorTmp, "2.png")
+    func:SetColor(colorTmp, colorTmp, colorTmp, "3.png")
 
-    func:SetOpacity(colorTmp * 255.0, "1.png");
-    func:SetOpacity(colorTmp * 255.0, "2.png");
-    func:SetOpacity(colorTmp * 255.0, "3.png");
+    func:SetOpacity(colorTmp * 255.0, "1.png")
+    func:SetOpacity(colorTmp * 255.0, "2.png")
+    func:SetOpacity(colorTmp * 255.0, "3.png")
 
     for i = 0, 4 do
-        func:DrawGraph((i * bgWidth_1) - bgScrollX_1, 0, "1.png");
-        func:DrawGraph((i * bgWidth_2) - bgScrollX_2, 0, "2.png");
-        func:DrawGraph((i * bgWidth_3) - bgScrollX_3, 0, "3.png");
+        func:DrawGraph((i * bgWidth_1) - bgScrollX_1, 0, "1.png")
+        func:DrawGraph((i * bgWidth_2) - bgScrollX_2, 0, "2.png")
+        func:DrawGraph((i * bgWidth_3) - bgScrollX_3, 0, "3.png")
     end
-    func:DrawGraph(0, 0, "6.png");
+    func:DrawGraph(0, 0, "6.png")
     for i = 0, 4 do
-        func:DrawGraph((i * bgWidth_4) - bgScrollX_4, -200 + bgScrollX_4, "4.png");
-        func:DrawGraph((i * bgWidth_5) - bgScrollX_5, 0, "5.png");
+        func:DrawGraph((i * bgWidth_4) - bgScrollX_4, -200 + bgScrollX_4, "4.png")
+        func:DrawGraph((i * bgWidth_5) - bgScrollX_5, 0, "5.png")
     end
 end
