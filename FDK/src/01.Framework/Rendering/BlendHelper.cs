911a99126c3d1f05500b4818fa6570534f1d2a4a
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2023-11-21 23:10:33 +0900

    Fix Multi and Screen blending methods (Fix black nameplate when gold dan title)

--- FDK/src/01.Framework/Rendering/BlendHelper.cs
+++ FDK/src/01.Framework/Rendering/BlendHelper.cs
@@ -18,25 +18,25 @@ public static class BlendHelper
         switch(blendType)
         {
             case BlendType.Normal:
-            Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
-            Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.OneMinusSrcAlpha);
-            break;
+                Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
+                Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.OneMinusSrcAlpha);
+                break;
             case BlendType.Add:
-            Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
-            Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.One);
-            break;
+                Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
+                Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.One);
+                break;
             case BlendType.Screen:
-            Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
-            Game.Gl.BlendFunc(GLEnum.OneMinusDstColor, GLEnum.One);
-            break;
+                Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
+                Game.Gl.BlendFunc(GLEnum.One, GLEnum.OneMinusSrcColor);
+                break;
             case BlendType.Multi:
-            Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
-            Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.SrcColor);
-            break;
+                Game.Gl.BlendEquation(BlendEquationModeEXT.FuncAdd);
+                Game.Gl.BlendFunc(GLEnum.DstColor, GLEnum.OneMinusSrcAlpha);
+                break;
             case BlendType.Sub:
-            Game.Gl.BlendEquation(BlendEquationModeEXT.FuncReverseSubtract);
-            Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.One);
-            break;
+                Game.Gl.BlendEquation(BlendEquationModeEXT.FuncReverseSubtract);
+                Game.Gl.BlendFunc(GLEnum.SrcAlpha, GLEnum.One);
+                break;
         }
     }
 }
