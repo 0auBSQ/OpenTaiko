e3ccd0d08d0617251f0845fab086dc7e65fdd59a
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2023-12-09 00:53:09 +0900

    fix dotnet install sh script for older linux distros

--- install-dotnet-sdk.sh
+++ install-dotnet-sdk.sh
@@ -1,1 +1,4 @@
-sudo apt install dotnet-sdk-7.0
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
