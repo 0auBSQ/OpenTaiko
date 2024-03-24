ac14d90d1e3171ead92f9e03aa402a4bf803054f
DragonRatTiger / リュウコ <dragonrattiger@gmail.com>
2024-03-23 22:51:15 -0500

    Update OWM assets (#583)

--- OpenTaiko/System/Open-World Memories/Graphics/5_Game/9_End/Tower_Dropout/Script.lua
+++ OpenTaiko/System/Open-World Memories/Graphics/5_Game/9_End/Tower_Dropout/Script.lua
@@ -6,21 +6,17 @@
 --func:SetScale(xscale, yscale, "filename");
 --func:SetColor(r, g, b, "filename");
 
-local x = { 758, 758, 758, 758, 758 }
 local y = { 218, 482, 0, 0, 0 }
 
 local animeCounter = { 0, 0, 0, 0, 0 }
 local nowFrame = { 0, 0, 0, 0, 0 }
 
-local textureCount = 25
+local slideInFrame = { 0, 0, 0, 0, 0 }
+local fallFrame = { 0, 0, 0, 0, 0 }
+local slideToFallFrameCount = 5
+local textureCount = 32
 
-local useExtraAnime = true
-
-function clearIn(player)
-end
-
-function clearOut(player)
-end
+local frameRate = 24
 
 function playEndAnime(player)
     animeCounter = { 0, 0, 0, 0, 0 }
@@ -30,37 +26,29 @@ end
 function init()
 
     if playerCount <= 2 then
-        y = { 217, 481, 0, 0, 0 }
+        y = { 216, 480, 0, 0, 0 }
     elseif playerCount == 5 then
-        y = { 34, 181, 397, 613, 829 }
+        y = { -5, 196, 412, 628, 844 }
     else
-        y = { -1, 262, 526, 790, 0 }
+        y = { -24, 240, 504, 768, 0 }
     end
 
     for i = 0 , textureCount do
-        func:AddGraph(tostring(i)..".png")
+        func:AddGraph(tostring(i) .. ".png")
     end
 
-    if useExtraAnime then
-        func:AddGraph("Clear_Failed.png")
-        func:AddGraph("Clear_Failed1.png")
-        func:AddGraph("Clear_Failed2.png")
-    end
 end
 
 function update(player)
-    animeCounter[player + 1] = animeCounter[player + 1] + (33.3 * deltaTime)
-    nowFrame[player + 1] = math.floor(animeCounter[player + 1] + 0.5)
+
+    animeCounter[player + 1] = animeCounter[player + 1] + deltaTime
+    slideInFrame[player + 1] = math.min(math.floor(animeCounter[player + 1] * frameRate), 23)
+    fallFrame[player + 1] = math.max(math.min(math.floor(((animeCounter[player + 1] - 1.133) * frameRate) + slideToFallFrameCount), 9), 0)
+
+    nowFrame[player + 1] = slideInFrame[player + 1] + fallFrame[player + 1]
+
 end
 
 function draw(player)
-    if nowFrame[player + 1] <= 20 or not(useExtraAnime) then
-        func:DrawGraph(x[player + 1], y[player + 1], tostring(math.min(nowFrame[player + 1], textureCount))..".png")
-    elseif nowFrame[player + 1] >= 20 and nowFrame[player + 1] <= 67 then
-        func:DrawGraph(x[player + 1] - 5, y[player + 1] + 71, "Clear_Failed.png")
-    elseif nowFrame[player + 1] == 68 then
-        func:DrawGraph(x[player + 1] - 5, y[player + 1] + 71, "Clear_Failed1.png")
-    elseif nowFrame[player + 1] >= 69 then
-        func:DrawGraph(x[player + 1] - 5, y[player + 1] + 71, "Clear_Failed2.png")
-    end
+    func:DrawGraph(500, y[player + 1] - 10, tostring(nowFrame[player + 1]) .. ".png")
 end
