4899dd4e6c579f553256fcef83f31a47ee219b2b
DragonRatTiger <dragonrattiger@gmail.com>
2024-03-27 04:18:07 -0400

    Don't allow Training Mode in Dan & Tower

--- OpenTaiko/src/Stages/05.DaniSelect/CActSelect段位挑戦選択画面.cs
+++ OpenTaiko/src/Stages/05.DaniSelect/CActSelect段位挑戦選択画面.cs
@@ -99,6 +99,7 @@ namespace TJAPlayer3
                         else if (n現在の選択行 == 1)
                         {
                             //TJAPlayer3.Skin.soundDanSongSelect.t再生する();
+                            TJAPlayer3.ConfigIni.bTokkunMode = false;
                             TJAPlayer3.Skin.soundDecideSFX.tPlay();
                             TJAPlayer3.Skin.voiceMenuDanSelectConfirm[TJAPlayer3.SaveFile]?.tPlay();
                             TJAPlayer3.stage段位選択.ct待機.Start(0, 3000, 1, TJAPlayer3.Timer);
