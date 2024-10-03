61f6f8b462bc0962036619522c6e247164362f21
DragonRatTiger / リュウコ <dragonrattiger@gmail.com>
2024-10-02 22:34:12 -0500

    Minor adjustments (#694)
    * fix trapped passion lyrics
    
    * Initialize Saves.db3 if it doesn't exist
    
    * how do i keep making this mistake everytime.

--- OpenTaiko/src/Common/OpenTaiko.cs
+++ OpenTaiko/src/Common/OpenTaiko.cs
@@ -2303,7 +2303,9 @@ for (int i = 0; i < 3; i++) {
 
 			VisualLogManager = new CVisualLogManager();
 
-
+			if (!File.Exists("Saves.db3")) {
+				File.Copy(@$".init{Path.DirectorySeparatorChar}Saves.db3", "Saves.db3");
+			}
 			// Add a condition here (if old Saves\ format save files exist) to port them to database (?)
 			SaveFileInstances = DBSaves.FetchSaveInstances();
 			/*
