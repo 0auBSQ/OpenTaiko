f3d4d1982c32046e4b10532c0cc21633806e6a8c
Julian Holfeld <morphclue@gmail.com>
2024-10-22 16:49:12 +0200

    refactor: CTextConsole

--- OpenTaiko/src/Common/CTextConsole.cs
+++ OpenTaiko/src/Common/CTextConsole.cs
@@ -17,24 +17,24 @@ namespace OpenTaiko {
 		// メソッド
 
 		public void tPrint(int x, int y, EFontType font, string strAlphanumericString) {
-			if (!base.IsDeActivated && !string.IsNullOrEmpty(strAlphanumericString)) {
-				int BOL = x;
-				for (int i = 0; i < strAlphanumericString.Length; i++) {
-					char ch = strAlphanumericString[i];
-					if (ch == '\n') {
-						x = BOL;
-						y += nFontHeight;
-					} else {
-						int index = str表記可能文字.IndexOf(ch);
-						if (index < 0) {
-							x += nFontWidth;
-						} else {
-							if (this.txフォント8x16[(int)((int)font / (int)EFontType.WhiteSlim)] != null) {
-								this.txフォント8x16[(int)((int)font / (int)EFontType.WhiteSlim)].t2D描画(x, y, this.rc文字の矩形領域[(int)((int)font % (int)EFontType.WhiteSlim), index]);
-							}
-							x += nFontWidth;
+			if (base.IsDeActivated || string.IsNullOrEmpty(strAlphanumericString)) {
+				return;
+			}
+
+			int BOL = x;
+			foreach (var ch in strAlphanumericString) {
+				if (ch == '\n') {
+					x = BOL;
+					y += this.nFontHeight;
+				} else {
+					int index = str表記可能文字.IndexOf(ch);
+					if (index >= 0) {
+						if (this.txフォント8x16[(int)((int)font / (int)EFontType.WhiteSlim)] != null) {
+							this.txフォント8x16[(int)((int)font / (int)EFontType.WhiteSlim)].t2D描画(x, y, this.rc文字の矩形領域[(int)((int)font % (int)EFontType.WhiteSlim), index]);
 						}
 					}
+
+					x += this.nFontWidth;
 				}
 			}
 		}
@@ -49,37 +49,41 @@ namespace OpenTaiko {
 			base.DeActivate();
 		}
 		public override void CreateManagedResource() {
-			if (!base.IsDeActivated) {
-				this.txフォント8x16[0] = OpenTaiko.Tx.TxC(@"Console_Font.png");
-				this.txフォント8x16[1] = OpenTaiko.Tx.TxC(@"Console_Font_Small.png");
-
-				nFontWidth = this.txフォント8x16[0].szTextureSize.Width / 32;
-				nFontHeight = this.txフォント8x16[0].szTextureSize.Height / 16;
-
-				this.rc文字の矩形領域 = new Rectangle[3, str表記可能文字.Length];
-				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < str表記可能文字.Length; j++) {
-						int regionX = nFontWidth * 16, regionY = nFontHeight * 8;
-						this.rc文字の矩形領域[i, j].X = ((i / 2) * regionX) + ((j % 16) * nFontWidth);
-						this.rc文字の矩形領域[i, j].Y = ((i % 2) * regionY) + ((j / 16) * nFontHeight);
-						this.rc文字の矩形領域[i, j].Width = nFontWidth;
-						this.rc文字の矩形領域[i, j].Height = nFontHeight;
-					}
-				}
-
-				base.CreateManagedResource();
+			if (base.IsDeActivated) {
+				return;
 			}
+
+			this.txフォント8x16[0] = OpenTaiko.Tx.TxC(@"Console_Font.png");
+			this.txフォント8x16[1] = OpenTaiko.Tx.TxC(@"Console_Font_Small.png");
+
+			this.nFontWidth = this.txフォント8x16[0].szTextureSize.Width / 32;
+			this.nFontHeight = this.txフォント8x16[0].szTextureSize.Height / 16;
+
+			this.rc文字の矩形領域 = new Rectangle[3, str表記可能文字.Length];
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < str表記可能文字.Length; j++) {
+					int regionX = this.nFontWidth * 16, regionY = this.nFontHeight * 8;
+					this.rc文字の矩形領域[i, j].X = ((i / 2) * regionX) + ((j % 16) * this.nFontWidth);
+					this.rc文字の矩形領域[i, j].Y = ((i % 2) * regionY) + ((j / 16) * this.nFontHeight);
+					this.rc文字の矩形領域[i, j].Width = this.nFontWidth;
+					this.rc文字の矩形領域[i, j].Height = this.nFontHeight;
+				}
+			}
+
+			base.CreateManagedResource();
 		}
 		public override void ReleaseManagedResource() {
-			if (!base.IsDeActivated) {
-				for (int i = 0; i < 2; i++) {
-					if (this.txフォント8x16[i] != null) {
-						this.txフォント8x16[i].Dispose();
-						this.txフォント8x16[i] = null;
-					}
-				}
-				base.ReleaseManagedResource();
+			if (base.IsDeActivated) {
+				return;
 			}
+
+			for (int i = 0; i < 2; i++) {
+				if (this.txフォント8x16[i] != null) {
+					this.txフォント8x16[i].Dispose();
+					this.txフォント8x16[i] = null;
+				}
+			}
+			base.ReleaseManagedResource();
 		}
 
 
