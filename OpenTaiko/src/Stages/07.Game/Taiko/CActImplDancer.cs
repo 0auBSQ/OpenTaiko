0auBSQ/OpenTaiko

commit 4eb7c80f070fc77125e8f28822509562b32630de

0.6.0.1 - Nameplates additions, minor fix
- Fix Mob, Dancer, Runner appearing despite empty array on the scene preset
- Chapter IV nameplates and make Chapter III all-songs nameplates accessible (condition changed from 999 songs to 34)
- Japanese translations for some nameplates

OpenTaiko/src/Stages/07.Game/Taiko/CActImplDancer.cs
@@ -35,7 +35,7 @@ internal class CActImplDancer : CActivity {
 		var dancerOrigindir = CSkin.Path($"{TextureLoader.BASE}{TextureLoader.GAME}{TextureLoader.DANCER}");
 		if (Directory.Exists($@"{dancerOrigindir}")) {
 			var dirs = Directory.GetDirectories($@"{dancerOrigindir}");
-			if (preset.DancerSet != null) {
+			if (preset.DancerSet.Length > 0 && preset.DancerSet != null) {
 				var _presetPath = (preset.DancerSet.Length > 0) ? $@"{dancerOrigindir}" + preset.DancerSet[random.Next(0, preset.DancerSet.Length)] : "";
 				var path = (Directory.Exists(_presetPath))
 					? _presetPath

OpenTaiko/src/Stages/07.Game/Taiko/CActImplMob.cs
@@ -20,7 +20,7 @@ internal class CActImplMob : CActivity {
 			Random random = new Random();
 
 			var upDirs = System.IO.Directory.GetDirectories(mobDir);
-			if (preset.MobSet != null) {
+			if (preset.MobSet.Length > 0 && preset.MobSet != null) {
 				var _presetPath = (preset.MobSet.Length > 0) ? $@"{mobDir}" + preset.MobSet[random.Next(0, preset.MobSet.Length)] : "";
 				var path = Directory.Exists(_presetPath)
 					? _presetPath

OpenTaiko/src/Stages/07.Game/Taiko/CActImplRunner.cs
@@ -62,7 +62,7 @@ internal class CActImplRunner : CActivity {
 		var dancerOrigindir = CSkin.Path($"{TextureLoader.BASE}{TextureLoader.GAME}{TextureLoader.RUNNER}");
 		if (Directory.Exists($@"{dancerOrigindir}")) {
 			var dirs = Directory.GetDirectories($@"{dancerOrigindir}");
-			if (preset.RunnerSet != null) {
+			if (preset.RunnerSet.Length > 0 && preset.RunnerSet != null) {
 				var _presetPath = (preset.RunnerSet.Length > 0) ? $@"{dancerOrigindir}" + preset.RunnerSet[random.Next(0, preset.RunnerSet.Length)] : "";
 				var path = (Directory.Exists(_presetPath))
 					? _presetPath

commit 3038a5d8873197f881d302fa0cf2f2dc375df32b

fix crash due to undefined mob, dancer, & runner, introduced in 4eb7c80f070fc77125e8f28822509562b32630de

OpenTaiko/src/Stages/07.Game/Taiko/CActImplDancer.cs
@@ -35,7 +35,7 @@ internal class CActImplDancer : CActivity {
 		var dancerOrigindir = CSkin.Path($"{TextureLoader.BASE}{TextureLoader.GAME}{TextureLoader.DANCER}");
 		if (Directory.Exists($@"{dancerOrigindir}")) {
 			var dirs = Directory.GetDirectories($@"{dancerOrigindir}");
-			if (preset.DancerSet.Length > 0 && preset.DancerSet != null) {
+			if (preset.DancerSet?.Length > 0) {
 				var _presetPath = (preset.DancerSet.Length > 0) ? $@"{dancerOrigindir}" + preset.DancerSet[random.Next(0, preset.DancerSet.Length)] : "";
 				var path = (Directory.Exists(_presetPath))
 					? _presetPath

OpenTaiko/src/Stages/07.Game/Taiko/CActImplMob.cs
@@ -20,7 +20,7 @@ internal class CActImplMob : CActivity {
 			Random random = new Random();
 
 			var upDirs = System.IO.Directory.GetDirectories(mobDir);
-			if (preset.MobSet.Length > 0 && preset.MobSet != null) {
+			if (preset.MobSet?.Length > 0) {
 				var _presetPath = (preset.MobSet.Length > 0) ? $@"{mobDir}" + preset.MobSet[random.Next(0, preset.MobSet.Length)] : "";
 				var path = Directory.Exists(_presetPath)
 					? _presetPath

OpenTaiko/src/Stages/07.Game/Taiko/CActImplRunner.cs
@@ -62,7 +62,7 @@ internal class CActImplRunner : CActivity {
 		var dancerOrigindir = CSkin.Path($"{TextureLoader.BASE}{TextureLoader.GAME}{TextureLoader.RUNNER}");
 		if (Directory.Exists($@"{dancerOrigindir}")) {
 			var dirs = Directory.GetDirectories($@"{dancerOrigindir}");
-			if (preset.RunnerSet.Length > 0 && preset.RunnerSet != null) {
+			if (preset.RunnerSet?.Length > 0) {
 				var _presetPath = (preset.RunnerSet.Length > 0) ? $@"{dancerOrigindir}" + preset.RunnerSet[random.Next(0, preset.RunnerSet.Length)] : "";
 				var path = (Directory.Exists(_presetPath))
 					? _presetPath