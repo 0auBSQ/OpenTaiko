03198193abea97d88758b7fe3686208213513bcb
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2024-12-04 13:38:22 +0800

    feat: detect and hide screen-obscuring rolls when any tips are out of screen

--- OpenTaiko/src/Stages/07.Game/CStage演奏画面共通.cs
+++ OpenTaiko/src/Stages/07.Game/CStage演奏画面共通.cs
@@ -4512,6 +4512,7 @@ internal abstract class CStage演奏画面共通 : CStage {
 
 			dTX.listChip[i].bHit = false;
 			dTX.listChip[i].bShow = true;
+			dTX.listChip[i].bShowRoll = true;
 			dTX.listChip[i].bProcessed = false;
 			dTX.listChip[i].bVisible = true;
 			dTX.listChip[i].IsHitted = false;
