e489de89a566e4f5a608eefce189a26dc64aa447
DragonRatTiger / リュウコ <dragonrattiger@gmail.com>
2025-01-16 02:18:47 -0600

    0.6.0.39 - Minor updates (#783)
    - The default font for most languages (when a skin doesn't include a font for that language) has been set to Noto Sans JP
    - The font size for the Offset text on the Calibration screen can now be adjusted
    - Fixed the user rename screen being off-center for resolutions other than 1920x1080
    - Lua security update

---
+++ OpenTaiko/src/Lua/LuaSecurity.cs
@@ -0,0 +1,28 @@
+﻿using NLua;
+
+namespace OpenTaiko {
+	public static class LuaSecurity {
+		public static void Secure(Lua lua) {
+
+			lua.DoString(@"
+for k, _ in pairs(os) do
+	if k ~= ""time"" then
+	os[k] = nil
+	end
+end
+for k, _ in pairs(io) do
+	io[k] = nil
+end
+io = nil
+for k, _ in pairs(debug) do
+	debug[k] = nil
+end
+debug = nil
+
+while #package.searchers > 0 do
+    table.remove(package.searchers);
+end
+");
+		}
+	}
+}
