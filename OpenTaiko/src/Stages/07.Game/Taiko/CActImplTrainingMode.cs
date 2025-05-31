1379b28cfd11c5d000ef5e43da60e4fb718f98e7
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2025-06-01 06:40:44 +0800

    refactor: make Easing functions static

--- OpenTaiko/src/Stages/07.Game/Taiko/CActImplTrainingMode.cs
+++ OpenTaiko/src/Stages/07.Game/Taiko/CActImplTrainingMode.cs
@@ -204,7 +204,7 @@ class CActImplTrainingMode : CActivity {
 				this.tToggleBookmarkAtTheCurrentPosition();
 
 			if (this.bCurrentlyScrolling) {
-				int msTargetTime = easing.EaseOut(this.ctScrollCounter, (int)this.nスクロール前ms, (int)this.nスクロール後ms, Easing.CalcType.Circular);
+				int msTargetTime = Easing.EaseOut(this.ctScrollCounter, (int)this.nスクロール前ms, (int)this.nスクロール後ms, Easing.CalcType.Circular);
 
 				this.ctScrollCounter.Tick();
 
@@ -437,7 +437,6 @@ class CActImplTrainingMode : CActivity {
 
 	private CCounter ctScrollCounter;
 	private CCounter ctBackgroundScrollTimer;
-	private Easing easing = new Easing();
 	private long length = 1; // chart length in TJA time
 
 	private List<int> gogoXList;
