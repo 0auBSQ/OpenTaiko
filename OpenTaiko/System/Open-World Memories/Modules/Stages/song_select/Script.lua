0b3cfe5541a9bbc21e0ff29a91cf919f1cf3e7b8
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2026-02-16 23:38:01 +0900

    pretty cool stuff

--- OpenTaiko/System/Open-World Memories/Modules/Stages/song_select/Script.lua
+++ OpenTaiko/System/Open-World Memories/Modules/Stages/song_select/Script.lua
@@ -17,13 +17,18 @@ local bgtx = {}
 
 local favoriteicon = nil
 
+-- Animations and counters
 local ctx = {}
 
 local currentBackground = 0
 local backgroundScrollX = 0
 
+local levelLabelFrame = 0
 local difficultyFade4 = 0
 
+local arrowsDistance = 0
+
+-- ???
 local difficultySelection = false
 local diffIndex = {-2, -2, -2, -2, -2}
 
@@ -35,6 +40,10 @@ local SONGLIST_OFFSET_Y = 120
 local SONGLIST_TEXT_OFFSET_X = -65
 local SONGLIST_TEXT_OFFSET_Y = 15
 local SONGLIST_SELECTED_X_DIFF = 50
+local SONGLIST_SELECTED_ARROW_GAP = 925
+
+local SONGBAR_LEVEL_EX_ANIMATION_FRAMECOUNT = 20
+local SONGBAR_LABEL_X_OFFSET = 288
 
 local SONGINFO_DIFFICULTIES_ORIGIN_X = 1790
 local SONGINFO_DIFFICULTIES_ORIGIN_Y = 154
@@ -58,6 +67,96 @@ local NAMEPLATE_BOX_START_X = 0
 local NAMEPLATE_BOX_SPACING_X = 384
 local NAMEPLATE_OFFSET_X = 27
 local NAMEPLATE_OFFSET_Y = 37
+
+-- Used for difficulty number on song bars, no texture/number if nil
+local function getSongNodeFocusChart(songNode)
+	if songNode.IsSong ~= true then
+		return nil
+	end
+	local default = math.min(4, CONFIG:GetDefaultCourse(0))
+	local chart = songNode:GetChart(default)
+	local i = 4
+	while chart == nil and i >= 0 do
+		chart = songNode:GetChart(i)
+		i = i-1
+	end
+	return chart
+end
+
+local function calculateNumberWidth(nb, txstr)
+    local str = tostring(nb)
+    local totalWidth = 0
+    local prevWidth = 0
+    
+    for i = 1, #str do
+        local digit = string.sub(str, i, i)
+        local tex = bgtx[txstr .. digit]
+        
+        if tex then
+            local w = tex.Width
+            
+            if i == 1 then
+                totalWidth = w
+            else
+                -- overlap offset (50%)
+                totalWidth = totalWidth + (prevWidth * 0.5)
+            end
+            prevWidth = w
+        end
+    end
+    return totalWidth
+end
+
+local function drawNumberCentered(nb, txstr, x, y, color)
+	color = color or COLOR:CreateColorFromHex("ffffffff")
+
+    local str = tostring(nb)
+    local totalWidth = calculateNumberWidth(nb, txstr)
+    
+    local cursorX = x - (totalWidth / 2)
+    local prevWidth = 0
+    
+    for i = 1, #str do
+        local digit = string.sub(str, i, i)
+        local tex = bgtx[txstr .. digit]
+        
+        if tex then
+            local w = tex.Width
+            if i == 1 then
+            else
+                -- overlap offset (50%)
+                cursorX = cursorX + (prevWidth * 0.5)
+            end
+			tex:SetColor(color)
+            tex:DrawAtAnchor(cursorX + (w / 2), y, "center")
+            prevWidth = w
+        end
+    end
+end
+
+local function drawLevelTag(songNode, x, y)
+	local chart = getSongNodeFocusChart(songNode)
+	if chart == nil then
+		return nil
+	end
+	local level = chart.Level
+	local difficulty = chart.DifficultyAsInt
+	local labelH = bars["levellabels"].Height/5
+	local labelW = bars["levellabels"].Width
+	if difficulty < 3 or level <= 10 then
+		bars["levellabels"]:DrawRectAtAnchor(x, y, 0, labelH*difficulty, labelW, labelH, "center")
+	elseif difficulty == 3 then
+		bars["levellabelsfire"]:DrawRectAtAnchor(x, y, 0, labelH*levelLabelFrame, labelW, labelH, "center")
+		drawNumberCentered(level, "levellabelsborder", x, y, COLOR:CreateColorFromHex("ffac0c0c"))
+	else
+		bars["levellabelsstorm"]:DrawRectAtAnchor(x, y, 0, labelH*levelLabelFrame, labelW, labelH, "center")
+		drawNumberCentered(level, "levellabelsborder", x, y, COLOR:CreateColorFromHex("ff83159e"))
+	end
+	drawNumberCentered(level, "levellabels", x, y)
+	if chart.IsPlus == true then
+		bars["levellabelsplus"]:DrawRectAtAnchor(x, y, 0, labelH*difficulty, labelW, labelH, "center")
+	end
+end
 
 local function reloadPreimage(songNode)
 	if songNode.IsSong == true then 
@@ -230,6 +329,8 @@ function draw()
 						bars["bar"]:DrawAtAnchor(xpos,ypos,"center")
 						genre_overlays[currentPage[i].Genre]:DrawAtAnchor(xpos,ypos,"center")
 					end
+					-- TODO: Don't show if locked AND grayed/blurred
+					drawLevelTag(currentPage[i],xpos+SONGBAR_LABEL_X_OFFSET,ypos)
 				elseif currentPage[i].IsRandom then
 					bars["random"]:DrawAtAnchor(xpos,ypos,"center")
 				elseif currentPage[i].IsReturn then
@@ -241,7 +342,10 @@ function draw()
 		local x0 = SONGLIST_ORIGIN_X+SONGLIST_SELECTED_X_DIFF
 		local y0 = SONGLIST_ORIGIN_Y
 		bars["selected"]:DrawAtAnchor(x0,y0,"center")
-		bars["selected-arrows"]:DrawAtAnchor(x0,y0,"center")
+		local xlshift = arrowsDistance * math.cos(7*math.pi/12)
+		local ylshift = arrowsDistance * math.sin(7*math.pi/12)
+		bars["selected-arrow-l"]:DrawAtAnchor(x0-SONGLIST_SELECTED_ARROW_GAP/2+xlshift,y0-ylshift,"left")
+		bars["selected-arrow-r"]:DrawAtAnchor(x0+SONGLIST_SELECTED_ARROW_GAP/2-xlshift,y0+ylshift,"right")
 	end
 
 	-- Folder Path
@@ -361,9 +465,8 @@ function update()
 end
 
 function activate()
-	test = GetSaveFile(0)
 	textMenuState = textLarge:GetText("Select a song!")
-	textTex = textSmall:GetText("You've played " .. tostring(test.TotalPlaycount) .. " charts.\nHow many more will you play?")
+	-- textTex = textSmall:GetText("You've played " .. tostring(test.TotalPlaycount) .. " charts.\nHow many more will you play?")
 
 	sounds.Skip = SHARED:GetSharedSound("Skip")
 	sounds.Cancel = SHARED:GetSharedSound("Cancel")
@@ -404,6 +507,22 @@ function activate()
 		end
 	end)
 	ctx["selectbox_animation"]:Start()
+
+	-- Arrows animation
+	ctx["arrows_animation"] = COUNTER:CreateCounter(10, 0, 1 / 10)
+	ctx["arrows_animation"]:SetBounce(true)
+	ctx["arrows_animation"]:Listen(function(val)
+		arrowsDistance = val
+	end)
+	ctx["arrows_animation"]:Start()
+
+	-- Special leveltag animation
+	ctx["leveltag_animation"] = COUNTER:CreateCounter(SONGBAR_LEVEL_EX_ANIMATION_FRAMECOUNT, 0, 1 / SONGBAR_LEVEL_EX_ANIMATION_FRAMECOUNT*2)
+	ctx["leveltag_animation"]:SetLoop(true)
+	ctx["leveltag_animation"]:Listen(function(val)
+		levelLabelFrame = math.floor(val)
+	end)
+	ctx["leveltag_animation"]:Start()
 end
 
 function deactivate()
@@ -420,6 +539,7 @@ function onStart()
 	text = TEXT:Create(28)
 	textLarge = TEXT:Create(42)
 	
+	-- General textures
 	SHARED:SetSharedTexture("background", "Textures/bg0.png")
 	bgtx["overlay"] = TEXTURE:CreateTexture("Textures/bg_overlay.png")
 	bgtx["songinfo"] = TEXTURE:CreateTexture("Textures/bg_songinfo.png")
@@ -433,12 +553,25 @@ function onStart()
 	for i = 0, 4, 1 do
 		bgtx["sinfo_difficulties_"..i] = TEXTURE:CreateTexture("Textures/sinfo_difficulties_"..i..".png")
 	end
+	for i = 0, 9, 1 do
+		bgtx["levellabels"..i] = TEXTURE:CreateTexture("Textures/BarLevel/"..i..".png")
+	end
+	for i = 0, 9, 1 do
+		bgtx["levellabelsborder"..i] = TEXTURE:CreateTexture("Textures/BarLevelBorder/"..i..".png")
+	end
+
+	-- Song list textures
 	bars["bar"] = TEXTURE:CreateTexture("Textures/bar.png")
 	bars["random"] = TEXTURE:CreateTexture("Textures/random.png")
 	bars["back"] = TEXTURE:CreateTexture("Textures/back.png")
 	bars["locked"] = TEXTURE:CreateTexture("Textures/locked.png")
 	bars["selected"] = TEXTURE:CreateTexture("Textures/selected.png")
-	bars["selected-arrows"] = TEXTURE:CreateTexture("Textures/selected-arrows.png")
+	bars["selected-arrow-l"] = TEXTURE:CreateTexture("Textures/selected-arrow-l.png")
+	bars["selected-arrow-r"] = TEXTURE:CreateTexture("Textures/selected-arrow-r.png")
+	bars["levellabels"] = TEXTURE:CreateTexture("Textures/bar_levelbg.png")
+	bars["levellabelsplus"] = TEXTURE:CreateTexture("Textures/bar_levelbgplus.png")
+	bars["levellabelsfire"] = TEXTURE:CreateTexture("Textures/bar_levelbgfire.png")
+	bars["levellabelsstorm"] = TEXTURE:CreateTexture("Textures/bar_levelbgstorm.png")
 
 	favoriteicon = TEXTURE:CreateTexture("Textures/fav.png")
 
