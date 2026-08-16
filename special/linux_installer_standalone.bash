11921c52eeff099c723a3d6d1699b1c3883ec9bd
Chopper <43015696+Chopper1337@users.noreply.github.com>
2026-08-16 18:15:12 +0100

    Prevent cloning git history on update

--- special/linux_installer_standalone.bash
+++ special/linux_installer_standalone.bash
@@ -80,7 +80,7 @@ cd "publish" || exit 1
 cached_soundtrack="$cache_dir/OpenTaiko-Soundtrack"
 if [ -d "$cached_soundtrack" ]; then
     echo "Updating cached soundtrack repository..."
-    cd "$cached_soundtrack" && git pull && cd - || exit 1
+    cd "$cached_soundtrack" && git fetch --depth 1 origin HEAD && git reset --hard FETCH_HEAD && cd - || exit 1
 else
     echo "Cloning soundtrack repository to cache..."
     git clone --depth 1 https://github.com/OpenTaiko/OpenTaiko-Soundtrack "$cached_soundtrack"
@@ -148,7 +148,7 @@ if pgrep -x "OpenTaiko" > /dev/null; then
 fi
 
 echo "Updating OpenTaiko repositories..."
-cd "$cache_dir/OpenTaiko-Soundtrack" && git pull
+cd "$cache_dir/OpenTaiko-Soundtrack" && git fetch --depth 1 origin HEAD && git reset --hard FETCH_HEAD
 cp -rf "$cache_dir/OpenTaiko-Soundtrack/"* "$installation_folder/publish/Songs/"
 cd "$cache_dir/OpenTaiko-Skins" && git pull
 cp -rf "$cache_dir/OpenTaiko-Skins/"* "$installation_folder/publish/"
